86316604faab8509004fb4602eb90721ced28b9c
Jonathan Zong <jonathan@jonathanzong.com>
2017-02-22 20:44:58 +0000

    intro

--- scripts/src/main.js
+++ scripts/src/main.js
@@ -4,37 +4,43 @@ $(function(){
   // http://www.lombardinetworks.net/lombardi.owl
   // convention: map wordcased name to css class
 
-  var load = localStorage.getItem('LOMBARDI_NODES')
-  if (load) {
-    load = JSON.parse(load);
-    Object.keys(nodes).forEach(function(node) {
-      nodes[node] = load[node];
-    });
+  nodes = {
+    "You": {
+      "name": "You",
+      "type": "person",
+      "x": 500.579138906468,
+      "y": 338.20016299542,
+      "px": 500.579138906468,
+      "py": 338.20016299542,
+      "neighbors": [],
+      "fixed": true
+    },
+    "Donald Trump": {
+      "name": "Donald Trump",
+      "type": "person",
+      "x": 642.4480837322,
+      "y": 338.20016299542,
+      "px": 642.4480837322,
+      "py": 338.20016299542,
+      "neighbors": [],
+      "fixed": true
+    }
   }
+  nodes["You"].neighbors.push(nodes["Donald Trump"]);
+  nodes["Donald Trump"].neighbors.push(nodes["You"]);
+  links = [{
+    source: nodes["You"],
+    target: nodes["Donald Trump"],
+    type: LINK_TYPE.FinancialTransaction
+  }];
 
-  var missing = {};
-  // Compute the distinct nodes from the links.
-  links.forEach(function(link) {
-    // link.source = nodes[link.source] || (nodes[link.source] = {name: link.source, type: NODE_TYPE.Organization});
-    // link.target = nodes[link.target] || (nodes[link.target] = {name: link.target, type: NODE_TYPE.Person});
-    if (!nodes[link.source]) {
-      // data isn't populated correctly
-      missing[link.source] = true;
-    }
-    if (!nodes[link.target]) {
-      // data isn't populated correctly
-      missing[link.target] = true;
-    }
-    link.source = nodes[link.source];
-    link.target = nodes[link.target];
-    if (!link.source.neighbors) link.source.neighbors = [];
-    if (!link.target.neighbors) link.target.neighbors = [];
-    link.source.neighbors.push(link.target);
-    link.target.neighbors.push(link.source);
-  });
-  missing = Object.keys(missing);
-  if (missing.length) {
-    console.log(missing);
+  function expandGraph() {
+    var a1 = $(nodes["You"]).animate({x:21, px: 21});
+    var a2 = $(nodes["Donald Trump"]).animate({x:1121, px: 1121});
+    $.when( a1, a2 ).done(function() {
+      initData();
+      restart();
+    });
   }
 
   var width = window.innerWidth, height = window.innerHeight;
@@ -49,6 +55,17 @@ $(function(){
       // .attr("height", height);
       .attr("viewBox", "0 0 1152 737")
       .attr("preserveAspectRatio", "xMidYMid meet");
+
+  $(".svg-container svg").one('click', function(){
+    expandGraph();
+    $(this).svgPanZoom({
+      events: {
+        mouseWheel: false,
+        doubleClick: false
+      },
+    });
+    $(this).addClass("pannable");
+  });
 
 
   // TODO association should be bidirectional
@@ -87,117 +104,96 @@ $(function(){
 
   restart();
 
-function restart() {
+  function restart() {
+    // Apply the general update pattern to the nodes.
+    node = node.data(d3.values(nodes), function(d) { return d.name;});
+    node.exit().remove();
 
-  // Apply the general update pattern to the nodes.
-  node = node.data(d3.values(nodes), function(d) { return d.name;});
+    node = node.enter().append("g")
+        .attr("class", function(d) { return "node " + d.type; });
+    node.append("circle")
+        .attr("r", 30);
+    node.append("text")
+        .attr("text-anchor", "middle")
+        .attr("dy", 0.3)
+        .text(function(d) { return d.name; })
+        .call(wrap, 50);
+    node = node.merge(node);
 
-  // node.exit().transition()
-  //     .selectAll("circle").attr("r", 0) ///////
-      // .remove();
-  node.exit().remove();
+    // Apply the general update pattern to the links.
+    link = link.data(links, function(d) { return d.source.name + "-" + d.target.name; });
 
-  node = node.enter().append("g")
-      .attr("class", function(d) { return "node " + d.type; });
-  node.append("circle")
-      .call(function(node) { node.transition().attr("r", 30); })
-  node.append("text")
-      .attr("text-anchor", "middle")
-      .attr("dy", 0.3)
-      .text(function(d) { return d.name; })
-      .call(wrap, 50);
-  node = node.merge(node);
+    // Keep the exiting links connected to the moving remaining nodes.
+    link.exit().transition()
+        .attr("stroke-opacity", 0)
+        .remove();
 
-  // Apply the general update pattern to the links.
-  link = link.data(links, function(d) { return d.source.name + "-" + d.target.name; });
+    link = link.enter().append("path")
+        .attr("class", function(d) { return "link " + d.type; })
+        .attr("marker-end", function(d) {
+          if (d.type == LINK_TYPE.Association ||
+               d.type == LINK_TYPE.FinancialServiceProvider ||
+               d.type == LINK_TYPE.InfluenceControl ||
+               d.type == LINK_TYPE.FinancialTransaction) {
+            return "url(#markerend)";
+          }
+        })
+        .attr("marker-start", function(d) {
+          if (d.type == LINK_TYPE.Association ||
+              d.type == LINK_TYPE.FinancialServiceProvider) {
+            return "url(#markerstart)";
+          }
+        })
+        .call(function(link) { link.transition().attr("stroke-opacity", 1); })
+      .merge(link);
 
-  // Keep the exiting links connected to the moving remaining nodes.
-  link.exit().transition()
-      .attr("stroke-opacity", 0)
-      .remove();
-
-  link = link.enter().append("path")
-      .attr("class", function(d) { return "link " + d.type; })
-      .attr("marker-end", function(d) {
-        if (d.type == LINK_TYPE.Association ||
-             d.type == LINK_TYPE.FinancialServiceProvider ||
-             d.type == LINK_TYPE.InfluenceControl ||
-             d.type == LINK_TYPE.FinancialTransaction) {
-          return "url(#markerend)";
+     node.on('mouseover', function(d) {
+      var toPath = [d.name]; // TODO cache this stuff
+      for (var i = 0; i < d.neighbors.length; i++) {
+        if (d.neighbors[i].type == NODE_TYPE.Action) {
+          toPath.push(d.neighbors[i].name);
         }
-      })
-      .attr("marker-start", function(d) {
-        if (d.type == LINK_TYPE.Association ||
-            d.type == LINK_TYPE.FinancialServiceProvider) {
-          return "url(#markerstart)";
-        }
-      })
-      .call(function(link) { link.transition().attr("stroke-opacity", 1); })
-    .merge(link);
-
-
-
-  node.on('mouseover', function(d) {
-    var toPath = [d.name];
-    for (var i = 0; i < d.neighbors.length; i++) {
-      if (d.neighbors[i].type == NODE_TYPE.Action) {
-        toPath.push(d.neighbors[i].name);
-      }
-      else {
-        if (d.neighbors[i].name !== "Donald Trump") {
-          var yPath = shortestPath(d.neighbors[i], nodes["You"], "Donald Trump");
-          toPath.push.apply(toPath, yPath);        
-        }
-        if (d.neighbors[i].name !== "You") {
-          var tPath = shortestPath(d.neighbors[i], nodes["Donald Trump"], "You");
-          toPath.push.apply(toPath, tPath);
+        else {
+          if (d.neighbors[i].name !== "Donald Trump") {
+            var yPath = shortestPath(d.neighbors[i], nodes["You"], "Donald Trump");
+            toPath.push.apply(toPath, yPath);        
+          }
+          if (d.neighbors[i].name !== "You") {
+            var tPath = shortestPath(d.neighbors[i], nodes["Donald Trump"], "You");
+            toPath.push.apply(toPath, tPath);
+          }
         }
       }
-    }
-    link.style('opacity', function(l) {
-      if (toPath.indexOf(l.source.name) >= 0 && toPath.indexOf(l.target.name) >= 0) {
-        return 1;
-      }
-      else {
-        return 0.1;
-      }
+      link.style('opacity', function(l) {
+        if (toPath.indexOf(l.source.name) >= 0 && toPath.indexOf(l.target.name) >= 0) {
+          return 1;
+        }
+        else {
+          return 0.1;
+        }
+      });
+      node.style('opacity', function(n) {
+        return toPath.indexOf(n.name) >= 0 ? 1 : 0.2;
+      });
+      // $('.ui-info').text(d.name + ' insert citation / explanation here'); // TODO
     });
-    node.style('opacity', function(n) {
-      return toPath.indexOf(n.name) >= 0 ? 1 : 0.2;
+
+    // Set the stroke width back to normal when mouse leaves the node.
+    node.on('mouseout', function() {
+      link.style('opacity', '');
+      node.style('opacity', '');
+      $('.ui-info').text('');
     });
-    // $('.ui-info').text(d.name + ' insert citation / explanation here'); // TODO
-  });
 
-  // Set the stroke width back to normal when mouse leaves the node.
-  node.on('mouseout', function() {
-    link.style('opacity', '');
-    node.style('opacity', '');
-    $('.ui-info').text('');
-  });
-
-  // Update and restart the simulation.
-  simulation.nodes(nodes);
-  simulation.force("link").links(links);
-  simulation.alpha(1).restart();
-  d3.timeout(function() {
-    simulation.stop();
-    console.log("stopped");
-  }, 1000);
-}
-
-d3.timeout(function() {
-  Object.keys(nodes).forEach(function (i) {
-    if (i !== 'You' && i !== 'Donald Trump') {
-      delete nodes[i];
-    }
-  });
-  links = [{
-    source: nodes["You"],
-    target: nodes["Donald Trump"],
-    type: LINK_TYPE.FinancialTransaction
-  }];
-  restart();
-}, 1000);
+    // Update and restart the simulation.
+    simulation.nodes(nodes);
+    simulation.force("link").links(links);
+    simulation.alpha(1).on("tick", tick).restart();
+    d3.timeout(function() {
+      simulation.stop();
+      console.log("stopped");
+    }, 5000);
+  }
 
   // Use elliptical arc path segments to doubly-encode directionality.
   function tick() {
@@ -246,15 +242,6 @@ d3.timeout(function() {
       text.attr("transform", "translate(0, " +(lineNumber * -7) + ")")
     });
   }
-
-
-  $(".svg-container svg").svgPanZoom({
-    events: {
-        mouseWheel: false,
-        doubleClick: false
-    },
-  });
-
 
 });
 
